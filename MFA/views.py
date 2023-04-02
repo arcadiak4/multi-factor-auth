006304782d45091b875a1bce370d95b58f595580
Aurore Duvernoy <aduvernoy@superprod.net>
2023-04-02 19:13:02 +0200

    Simple template, view, and url to login and for login success

--- MFA/views.py
+++ MFA/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.views.generic.edit import FormView
+
+from django.template.loader import render_to_string
+
+from .forms import (
+    LoginForm,
+)
+
+class LoginUser(FormView):
+    """FormView to log in
+    All form fields of CustomUser are sent to login.html
+    """
+
+    template_name = "multifactor/login.html"
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        """Goes to login_success.html
+        Returns user
+        """
+
+        return HttpResponse(
+            render_to_string(
+                "multifactor/login_success.html", {"username": form.cleaned_data['username']}
+            )
+        )
